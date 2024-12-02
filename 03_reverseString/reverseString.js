c59313e0edf1950591362eb293d1d0d393d655f9
inkyless <violalakwok@gmail.com>
2024-12-02 16:47:07 +0700

    Finished exercises halfway

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = [];
+    let len_string = string.length
+    let len = len_string
+    for (let i=0;i<len_string;i++){
+        len--
+        result.push(string[len])
+    }
+    return result.join('')
+    
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...numbers) {
+    let result = [];
+    for (eles of array){
+        if (numbers.includes(eles)){
+            continue;
+        } else{
+            result.push(eles)
+        }   
+        }
+        
+    return result
+
 };
 
 // Do not edit below this line
